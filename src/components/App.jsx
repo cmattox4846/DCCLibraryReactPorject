210d9d442bdca172135445de53ee557ad46b3e97
cmattox4846 <cmattox4846@gmail.com>
2021-11-02 17:12:59 -0400

    created goToNextBook +goToPreviousBook Method and buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,27 @@ class App extends Component{
         };
     }
 
+    goToNextBook=()=>{
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber===this.books.length){
+            tempBookNumber= 0 ;
+        }
+        this.setState({
+            bookNumber:tempBookNumber
+        });
+    }
+
+    goToPreviousBook=()=>{
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        if(tempBookNumber < 0){
+            tempBookNumber = this.books.length - 1; 
+        }
+        this.setState({
+            bookNumber:tempBookNumber
+        });
+    }
 
     render(){
         return(
@@ -26,6 +47,7 @@ class App extends Component{
                 <div className="row">
                     <div className="col-md-4">
                         {/* Button here for move to previous book */}
+                        <button onClick={this.goToPreviousBook}>Previous Book</button>
                     </div>
                     <div className="col-md-4">
                         <h1>{this.books[this.state.bookNumber].title}</h1>
@@ -34,6 +56,7 @@ class App extends Component{
                     </div>
                     <div className="col-md-4">
                         {/* Button here for move to next book */}
+                        <button onClick={this.goToNextBook}>Next Book</button>
                     </div>
                 </div>
             </div>
