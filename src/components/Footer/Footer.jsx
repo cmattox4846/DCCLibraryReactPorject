b29c490d11862af5621273239e5d4955f7e672b3
cmattox4846 <cmattox4846@gmail.com>
2021-11-02 17:53:45 -0400

    Added footer with sperate component file

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Footer.css"
+
+
+
+const footer = () => {
+    return ( 
+        <div className="row row-space">
+
+            <div className='footer'>
+                <h5> Chris'Library | Privacy Policy | Terms</h5>
+            </div>
+        </div>
+
+     );
+}
+ 
+export default footer;
